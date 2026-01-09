131e10dedbb2240b5e00b926f08f0d48f123589f
donotlook <repodsalgo200zzz@gmail.com>
2026-01-10 01:33:34 +0800

    feat: admin code generation verifying for Vue

--- urls.py
+++ urls.py
@@ -1,4 +1,4 @@
-from routes import items_bp, sales_bp, user_routes, payment_bp, cash_payment_bp, ml_bp, recommendations_bp
+from routes import items_bp, sales_bp, user_routes, payment_bp, cash_payment_bp, ml_bp, recommendations_bp, admin_cash_bp
 
 
 def register_routes(app):
@@ -7,5 +7,6 @@ def register_routes(app):
     app.register_blueprint(user_routes, url_prefix='/users')
     app.register_blueprint(payment_bp, url_prefix='/payment')
     app.register_blueprint(cash_payment_bp, url_prefix="/payment/cash")
+    app.register_blueprint(admin_cash_bp, url_prefix="/payment/admin/cash")
     app.register_blueprint(ml_bp, url_prefix='/ml')
     app.register_blueprint(recommendations_bp, url_prefix='/')
